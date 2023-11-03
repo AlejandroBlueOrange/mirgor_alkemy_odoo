e3dd7c5b7f7d65f229129d8f12670eee49c5d494
Alejandro Tacchella <atacchella@blueorange.com.ar>
2023-11-03 13:00:07 -0300

    [ADD] Módulo simple

---
+++ simple_module/models/simple_model.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import models, fields
+
+
+class SimpleModel(models.Model):
+    _name = 'simple.model'
+    _description = 'Simple Model'
+    
+    name = fields.Char(string='Name', help='This is a record name', required=True, default=lambda x: 'Name', index=True)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
